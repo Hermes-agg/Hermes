f9b76ba50f90b154a7ee3bcb01612b82502dad43
bigdreamsweb3 <italianloader@gmail.com>
2025-12-10 22:44:54 +0100

    Something just got updated!

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,13 +10,10 @@ export default function Home() {
 
 
   return (
-    <div className=" mx-auto max-w-7xl transition-opacity duration-300 flex flex-col items-center justify-start w-full flex-1 h-full mt-0! gap-8 ">
-      {/* <MarketStats /> */}
-
+    <div className="mx-auto max-w-7xl transition-opacity duration-300 flex flex-col items-center justify-start w-full flex-1 min-sm:px-1!  md:px-8 lg:mt-6 h-full mt-0! gap-8 3xl:px-20 md:pt-[10vh]">
       <YieldPanel />
-
-
       <MarketAnalytics />
+      {/* <MarketStats /> */}
 
 
 
